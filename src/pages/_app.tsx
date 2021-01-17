78af548bb06fe57bf6e066360bdd0cbcda145d0a
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2021-01-16 21:50:14 -0300

    feat: :ok_hand: added and configured styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,10 @@
+import GLobalStyle from '../styles/GlobalStyles'
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <GLobalStyle />
+    <Component {...pageProps} />
+    </>
+  )
 }
